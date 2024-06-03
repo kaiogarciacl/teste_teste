fb99d3954497ac03b4288c40f12030bbca3194dc
kaiogarciacl <kaio.garcia@clcaldeiraria.com.br>
2024-06-03 12:13:39 -0300

    main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="PROMETHEUS",layout="wide", initial_sidebar_state=
 with open("style.css") as f:st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 with st.sidebar:
-    st.image(fr'D:\img_prometheus\logo_prometeus.jpeg')
+    
     log = st.text_input('LOG', label_visibility='collapsed', placeholder='Login', key='Log')
     senha = st.text_input('senha', label_visibility='collapsed', placeholder='Senha', key='senha', type='password')
 
